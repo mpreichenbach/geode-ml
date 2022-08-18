b21763215665c4fa5880541007ff80eeaeff0d98
Matthew Reichenbach <matthew.p.reichenbach@erdc.dren.mil>
2022-08-18 14:37:37 -0600

    Added a test, test imagery.

---
+++ test/test_set_source_imagery.py
@@ -0,0 +1,26 @@
+import unittest
+from DeepLearningDatasets import SemanticSegmentation
+
+DATASET_DESCRIPTION = "An image over Bellingham, WA."
+CHANNEL_DESCRIPTION = "RGB"
+TEST_IMAGE_PATH = "test/imagery/"
+
+
+class TestSemanticSegmentation(unittest.TestCase):
+    """Unit tests for the set_source_imagery method of the SemanticSegmentation class."""
+
+    def setUp(self) -> None:
+        """Set up test fixtures."""
+        self.dataset = SemanticSegmentation(dataset_description=DATASET_DESCRIPTION,
+                                            channel_description=CHANNEL_DESCRIPTION)
+
+    def test_set_source_imagery(self):
+        """Test whether the set_source_imagery method works as expected."""
+        self.dataset.set_source_imagery(path=TEST_IMAGE_PATH)
+
+        self.assertEqual(self.dataset.source_path, TEST_IMAGE_PATH)
+        self.assertListEqual(self.dataset.source_images, ["bellingham_cropped.tif"])
+
+
+if __name__ == "__main__":
+    unittest.main()
