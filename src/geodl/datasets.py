63a430dbfd5fb1e2a7af69228c1d637ff37ceb6a
Matthew Reichenbach <matthew.p.reichenbach@erdc.dren.mil>
2022-10-03 15:06:56 -0400

    Moved attributes into init scope; expanded arguments of init.

--- src/geodl/datasets.py
+++ src/geodl/datasets.py
@@ -9,25 +9,65 @@ from pathlib import Path
 class SemSeg:
     """Defines a semantic segmentation dataset to be used in deep-learning models."""
 
-    labels: dict = {}
-    vector_path: str = ""
-    source_metadata: dict = {}
-    raster__path: str = ""
-    resampled_metadata: dict = {}
-    source_image_names: list = []
-    source_path: str = ""
-    tile_dimension: int = 0
-    tile_path: str = ""
-
     def __init__(self,
+                 source_path: str = "",
+                 vector_path: str = "",
+                 raster_path: str = "",
+                 tile_dimension: int = 0,
+                 tile_path: str = "",
                  dataset_description: str = "",
                  channel_description: str = ""):
 
         self.channel_description: str = channel_description
         self.dataset_description: str = dataset_description
+        self.raster__path = raster_path
+        self.resampled_metadata: dict = {}
+        self.source_image_names = os.listdir(source_path)
+        self.source_metadata: dict = {}
+        self.source_path = source_path
+        self.vector_path = vector_path
+        self.tile_dimension = tile_dimension
+        self.tile_path = tile_path
 
-    @classmethod
-    def check_source(cls) -> None:
+        # check whether the source imagery exists
+        self.check_source()
+
+        # loop through the source imagery and extract relevant metadata
+        for filename in self.source_image_names:
+            dst = gdal.Open(os.path.join(self.source_path, filename))
+
+            # set metadata fields from the gdal.Dataset object
+            metadata_dict = {}
+            gt = dst.GetGeoTransform()
+            x_dim = dst.RasterXSize
+            y_dim = dst.RasterYSize
+
+            metadata_dict["band_counts"] = dst.RasterCount
+            metadata_dict["dimensions"] = (x_dim, y_dim)
+            metadata_dict["resolution"] = (gt[1], gt[5])
+            metadata_dict["raster_extent"] = np.abs(x_dim * gt[1] * y_dim * gt[5])
+
+            # calculate the "data_extent", or the raster_extent minus the area of nodata pixels. First, compute the
+            # number of nodata pixels
+            n_nodata_pixels = np.sum(np.where(np.sum(dst.ReadAsArray()) == 0, 1, 0))
+
+            # then compute the area of these pixels
+            nodata_area = np.abs(n_nodata_pixels * x_dim * y_dim)
+
+            # set the data_extent in the metadata_dictionary
+            metadata_dict["data_extent"] = metadata_dict["raster_extent"] - nodata_area
+
+            if "metre" in dst.GetProjection():
+                metadata_dict["units"] = "metre"
+            else:
+                raise(Exception("The raster " + filename + " may not be in metres."))
+
+            self.source_metadata[filename] = metadata_dict
+
+            # close the gdal.Dataset object
+            dst = None
+
+    def check_source(self) -> None:
         """Checks whether the source imagery has been set, and is nonempty.
 
         Returns:
@@ -38,13 +78,12 @@ class SemSeg:
             Exception: if source_path has been set, but is empty.
         """
 
-        if cls.source_path == "":
-            raise Exception("The source_path has not been set; run the method set_source_imagery first.")
-        elif len(os.listdir(cls.source_path)) == 0:
+        if self.source_path == "":
+            raise Exception("The source_path has not been set.")
+        elif len(os.listdir(self.source_path)) == 0:
             raise Exception("The source_path is empty.")
 
-    @classmethod
-    def check_vectors(cls):
+    def check_vectors(self) -> None:
         """Checks whether the vector data has been set, and matches the names of the source imagery.
         
         Returns:
@@ -57,17 +96,17 @@ class SemSeg:
             Exception: if a vector data folder does not contain a shapefile.
         """
 
-        if cls.vector_path == "":
+        if self.vector_path == "":
             raise Exception("The vector_path has not been set; run either the get_label_vectors "
                             "or set_label_vectors first")
-        elif len(os.listdir(cls.vector_path)) == 0:
+        elif len(os.listdir(self.vector_path)) == 0:
             raise Exception("The vector_path is empty.")
-        elif [Path(x).stem for x in os.listdir(cls.source_path)] != os.listdir(cls.vector_path):
+        elif [Path(x).stem for x in os.listdir(self.source_path)] != os.listdir(self.vector_path):
             raise Exception("Source imagery names do not match vector data names.")
         else:
-            for directory in os.listdir(cls.source_path):
-                fnames = os.listdir(directory)
-                shapefiles = [x for x in fnames if "shp" in x]
+            for directory in os.listdir(self.source_path):
+                filenames = os.listdir(directory)
+                shapefiles = [x for x in filenames if "shp" in x]
                 if len(shapefiles) != 1:
                     raise Exception("A vector data directories must have exactly one shapefile.")
 
@@ -161,43 +200,3 @@ class SemSeg:
             None
         """
 
-        # update source_path attribute
-        self.source_path = source_path
-
-        # get imagery names
-        self.source_image_names = os.listdir(self.source_path)
-
-        # loop through the imagery and extract relevant metadata
-        for filename in self.source_image_names:
-            dst = gdal.Open(os.path.join(self.source_path, filename))
-
-            # set metadata fields from the gdal.Dataset object
-            metadata_dict = {}
-            gt = dst.GetGeoTransform()
-            x_dim = dst.RasterXSize
-            y_dim = dst.RasterYSize
-
-            metadata_dict["band_counts"] = dst.RasterCount
-            metadata_dict["dimensions"] = (x_dim, y_dim)
-            metadata_dict["resolution"] = (gt[1], gt[5])
-            metadata_dict["raster_extent"] = np.abs(x_dim * gt[1] * y_dim * gt[5])
-
-            # calculate the "data_extent", or the raster_extent minus the area of nodata pixels. First, compute the
-            # number of nodata pixels
-            n_nodata_pixels = np.sum(np.where(np.sum(dst.ReadAsArray()) == 0, 1, 0))
-
-            # then compute the area of these pixels
-            nodata_area = np.abs(n_nodata_pixels * x_dim * y_dim)
-
-            # set the data_extent in the metadata_dictionary
-            metadata_dict["data_extent"] = metadata_dict["raster_extent"] - nodata_area
-
-            if "metre" in dst.GetProjection():
-                metadata_dict["units"] = "metre"
-            else:
-                raise(Exception("The raster " + filename + " may not be in metres."))
-
-            self.source_metadata[filename] = metadata_dict
-
-            # close the gdal.Dataset object
-            dst = None
