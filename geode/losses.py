a6a6c81c91381538cb6c2a9ecf32f17dadae564c
Matthew Reichenbach <matthew.p.reichenbach@erdc.dren.mil>
2023-02-24 14:41:17 -0500

    Added casting to losses, updated documentation, updated version number.

--- geode/losses.py
+++ geode/losses.py
@@ -1,9 +1,19 @@
 # losses.py
 
+import tensorflow as tf
 from tensorflow.keras import backend as K
 
 
 def dice_loss(y_true, y_pred, smooth=100):
+    """Computes the dice-loss between one-hot encoded arrays, then returns a score between [0, 1].
+
+    Args:
+        y_true: tensor of ground-truth values of size (batch, height, width, n_classes;
+        y_pred: tensor of model predictions of size (batch, height, width, n_classes)."""
+
+    y_true = K.cast(y_true, dtype=tf.float32)
+    y_pred = K.cast(y_pred, dtype=tf.float32)
+
     y_true_f = K.flatten(y_true)
     y_pred_f = K.flatten(y_pred)
     intersection = K.sum(y_true_f * y_pred_f)
@@ -13,30 +23,36 @@ def dice_loss(y_true, y_pred, smooth=100):
     return 1 - dice
 
 
-def iou_loss(y_true, y_pred):
-    """Computes the IoU for a single target class, then return a loss score between [0, 1].
+def iou_loss(y_true, y_pred, smooth=100):
+    """Computes the IoU-loss between one-hot encoded arrays, then return a loss score between [0, 1].
 
     Args:
         y_true: tensor of ground-truth values of size (batch, height, width);
         y_pred: tensor of model predictions of size (batch, height, width)."""
-    # use tf.math.reduce_sum
+
+    y_true = K.cast(y_true, dtype=tf.float32)
+    y_pred = K.cast(y_pred, dtype=tf.float32)
+
     intersection = K.sum(K.flatten(y_true * y_pred))
-    union = K.sum(K.flatten(y_true + y_pred - y_true * y_pred)) + 1
+    union = K.sum(K.flatten(y_true + y_pred - y_true * y_pred)) + smooth
 
-    final = 1 - intersection / union
+    iou = intersection / union
 
-    return final
+    return 1 - iou
 
-def log_iou_loss(y_true, y_pred):
-    """Compute a variation of the -log(IoU) loss introduced in 'Unitbox': An Advanced Object Detection Network. This
-    version includes the 'smooth' parameter to ensure no division by zero.
+def log_iou_loss(y_true, y_pred, smooth=100):
+    """Computes a variation of the -log(IoU) loss introduced in 'Unitbox': An Advanced Object Detection Network, with
+    a smoothing parameter to (among other things) avoid division by zero.
 
     Args:
-        y_true: tensor of ground-truth values of size (batch, height, width), so not one-hot encoded;
-        y_pred: tensor of model predictions of size (batch, height, width, 2), so one-hot encoded."""
+        y_true: tensor of ground-truth values of size (batch, height, width);
+        y_pred: tensor of model predictions of size (batch, height, width)."""
 
-    intersection = K.sum(K.flatten(y_true * y_pred)) + 1
-    union = K.sum(K.flatten(y_true + y_pred - y_true * y_pred)) + 1
+    y_true = K.cast(y_true, dtype=tf.float32)
+    y_pred = K.cast(y_pred, dtype=tf.float32)
+
+    intersection = K.sum(K.flatten(y_true * y_pred)) + smooth
+    union = K.sum(K.flatten(y_true + y_pred - y_true * y_pred)) + smooth
 
     final = - K.log(intersection / union)
 
